f786109b334ebdfd86bdb2caecd83f14067a0db2
Raichur36 <anudeepahiremath@gmail.com>
2024-11-26 14:11:57 +0530

    Add files via upload

---
+++ positive and negative.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter the number");
+scanf("%d",&num);
+if("num>0")
+{
+printf("The number is positve");
+}
+else if("num<0")
+{
+printf("The number is negative");
+}
+else
+{
+printf("The number is zero");
+}
+return 0;
+}
